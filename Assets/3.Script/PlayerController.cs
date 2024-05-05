92cee6fd44cd06e8bdea05b30b8285af0ec23135
magicJSC <jeonghaejun004@gmail.com>
2024-05-05 23:37:43 +0900

    commit

---
+++ Assets/3.Script/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    void Update()
+    {
+        Move();   
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+    }
+}

fa5d16a7e96b70ab94d57465cdf401e8a46ad029
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2024-05-10 23:17:53 +0900

    Network

---
+++ Assets/3.Scripts/Packet/PacketHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Google.Protobuf;
+using ServerCore;
+using Protocol;
+using System;
+
+class PacketHandler
+{
+    public static void S_ChatHandler(PacketSession session, IMessage packet)
+    {
+        S_CHAT chatPacket = packet as S_CHAT;
+        ServerSession serverSession = session as ServerSession;
+
+        Debug.Log(chatPacket.ToString());
+    }
+
+    public static void S_EnterGameHandler(PacketSession session, IMessage packet)
+    {
+        S_ENTER_GAME enterGamePacket = packet as S_ENTER_GAME;
+        ServerSession serverSession = session as ServerSession;
+    }
+
+    public static void S_LeaveGameHandler(PacketSession session, IMessage packet)
+    {
+        S_LEAVE_GAME enterGamePacket = packet as S_LEAVE_GAME;
+        ServerSession serverSession = session as ServerSession;
+    }
+}
---
+++ Assets/3.Scripts/Packet/ServerSession.cs
@@ -0,0 +1,37 @@
+﻿using Google.Protobuf;
+using Protocol;
+using ServerCore;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class ServerSession : PacketSession
+{
+    public override void OnConnected(EndPoint endPoint)
+    {
+        if (endPoint == null)
+            return;
+
+        Debug.Log($"OnConnected : {endPoint}");
+    }
+
+    public override void OnDisconnected(EndPoint endPoint)
+    {
+        Debug.Log($"DisConnected : {endPoint}");
+    }
+
+    public override void OnRecvPacket(ArraySegment<byte> buffer)
+    {
+        S_CHAT pkt = new S_CHAT();
+        pkt.MergeFrom(buffer.Array, buffer.Offset + 4, buffer.Count - 4);
+
+        PacketHandler.S_ChatHandler(this, pkt);
+    }
+
+    public override void OnSend(int numOfBytes)
+    {
+        Debug.Log($"SendPacket : {numOfBytes}");
+    }
+}

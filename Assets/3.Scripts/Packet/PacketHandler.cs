1e83ec354a1b9cd579fbec6af4a5cf2ed541555b
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2024-05-16 23:47:13 +0900

    PacketHandler

--- Assets/3.Scripts/Packet/PacketHandler.cs
+++ Assets/3.Scripts/Packet/PacketHandler.cs
@@ -6,23 +6,26 @@ using System;
 
 class PacketHandler
 {
-    public static void SChatHandler(PacketSession session, IMessage packet)
-    {
-        S_CHAT chatPacket = packet as S_CHAT;
-        ServerSession serverSession = session as ServerSession;
-
-        Debug.Log(chatPacket.Msg);
-    }
-
     public static void SEnterGameHandler(PacketSession session, IMessage packet)
     {
         S_ENTER_GAME enterGamePacket = packet as S_ENTER_GAME;
         ServerSession serverSession = session as ServerSession;
+
+        Debug.Log("S_EnterGameHandler");
+        Debug.Log(enterGamePacket.Player);
     }
 
     public static void SLeaveGameHandler(PacketSession session, IMessage packet)
     {
         S_LEAVE_GAME enterGamePacket = packet as S_LEAVE_GAME;
         ServerSession serverSession = session as ServerSession;
+    }
+
+    public static void SChatHandler(PacketSession session, IMessage packet)
+    {
+        S_CHAT chatPacket = packet as S_CHAT;
+        ServerSession serverSession = session as ServerSession;
+
+        Debug.Log(chatPacket.Msg);
     }
 }
